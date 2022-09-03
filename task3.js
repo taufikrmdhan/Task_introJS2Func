8d84f2c9f7630fb0021737436ab4887662d1d43d
Taufik Ramadhan <taufik.30@students.amikom.ac.id>
2022-09-03 18:17:15 +0700

    update task3.js

--- task3.js
+++ task3.js
@@ -2,14 +2,18 @@ function seleksiNilai(nilaiAwal, nilaiAkhir, dataArray) {
   if (nilaiAwal < nilaiAkhir) {
     if (dataArray.length > 5) {
       let data = dataArray.filter((item) => {
-        return item > nilaiAwal && item < nilaiAkhir;
+        return item >= nilaiAwal && item <= nilaiAkhir;
       });
-      console.log(data.sort((a, b) => a - b));
+      if (data == "") {
+        console.log("Nilai tidak ditemukan");
+      } else {
+        console.log(data.sort((a, b) => a - b));
+      }
     } else {
-      return "Jumlah angka dalam dataArray harus lebih dari 5";
+      console.log("Jumlah angka dalam dataArray harus lebih dari 5");
     }
   } else {
-      return "Nilai akhir harus lebih besar dari nilai awal";
+    console.log("Nilai akhir harus lebih besar dari nilai awal");
   }
 }
-seleksiNilai(5, 20, [2, 25, 4, 14, 17, 30, 8]);
+seleksiNilai(4, 70, [2,  22, 9]);
